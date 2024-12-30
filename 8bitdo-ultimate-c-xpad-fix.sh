9cf1100fbb9ea19ad241f300f800d2740a897b31
Luke Moyer <174116256+DudenessBoy@users.noreply.github.com>
2024-12-30 11:01:06 -0500

    Fixed potential error if path doesn't exist

--- 8bitdo-ultimate-c-xpad-fix.sh
+++ 8bitdo-ultimate-c-xpad-fix.sh
@@ -5,5 +5,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+directory_path="/etc/udev/rules.d"
+if [ ! -d "$directory_path" ]; then
+    mkdir -p "$directory_path"
+fi
 echo "ACTION==\"add\", ATTRS{idVendor}==\"2dc8\", ATTRS{idProduct}==\"3106\", RUN+=\"/sbin/modprobe xpad\", RUN+=\"/bin/sh -c \'echo 2dc8 3106 > /sys/bus/usb/drivers/xpad/new_id\'" > /etc/udev/rules.d/99-8bitdo-xinput.rules
 udevadm control --reload
